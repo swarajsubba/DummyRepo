262f544cd870677f72d95b7d59b3f01f219262bf
Swaraj Subba <swaraj_subba@outlook.com>
2018-11-18 14:29:49 +0530

    Application Source

---
+++ Dummy/New Unity Project/Assets/Application/Scripts/APIHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class APIHandler : MonoBehaviour {
+    
+    string _authLink = "https://api.instagram.com/oauth/authorize/?client_id=9fc1ce590f154fb7a361cd446ceda74e"+"&redirect_uri=REDIRECT-URI&response_type=code";
+    string self = "https://api.instagram.com/v1/users/self/?access_token=";
+
+    string access_token = "282628530.9fc1ce5.edb208528cd546de91233993ea0f3641";
+     public string text;
+
+    private void Start ()
+    {
+        StartCoroutine (Auth());
+    }
+
+    IEnumerator Auth ()
+    {
+        WWWForm form = new WWWForm ();
+        Dictionary<string, string> headers = form.headers;
+        
+        Debug.Log ("No shit!");
+        using (UnityWebRequest www = new UnityWebRequest (self+access_token))
+        {
+            text = (www.downloadHandler.text);
+            if (www.isDone)
+            {
+                Debug.Log (www.downloadHandler.text);
+            }
+            yield return new WaitForEndOfFrame ();
+        }
+    }
+}
---
+++ Dummy/New Unity Project/Assets/Application/Scripts/UIHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHandler : MonoBehaviour {
+
+    public Canvas _canvas;
+    public RectTransform _content;
+    public RectTransform _tilePrefab;
+
+    [Tooltip("Temp variable, Delete afterwards")]
+    public int _numberOfTiles;
+
+    private void Start()
+    {
+        Invoke("CreateContentTiles",1f);
+    }
+
+    void CreateContentTiles() {
+        _content.sizeDelta = new Vector2 (1, _numberOfTiles *1080);
+        
+        for (int i=0; i < _numberOfTiles ; i++) {
+            RectTransform tile = Instantiate(_tilePrefab) as RectTransform;
+            tile.transform.parent = _content;
+            tile.transform.localScale = Vector3.one;
+            tile.anchoredPosition = new Vector3(0, -1080 * i, 1);
+            tile.GetComponent<Image> ().color = Random.ColorHSV ();
+            //tile.transform.position = 
+            Debug.Log("Created some tiles!");
+        }
+    }
+}
